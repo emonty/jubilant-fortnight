ee51abc495df37ebfa222b1f3e31f1805dfad753
Monty Taylor <mordred@inaugust.com>
2018-04-09 11:57:12 -0500

    Added font-awesome and image files

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,8 @@ import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
 import './all.sass'
+import "font-awesome/css/font-awesome.css"
+import "./main.css"
 
 const TemplateWrapper = ({ children }) => (
   <div>
